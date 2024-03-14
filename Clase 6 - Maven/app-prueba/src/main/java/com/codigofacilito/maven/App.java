c6aecb558e7d994e59375f5ed6a4875e3537cbb5
Jairo A. Robledo <jairoarpf91@gmail.com>
2024-03-13 21:59:14 -0300

    class6 Maven

---
+++ Clase 6 - Maven/app-prueba/src/main/java/com/codigofacilito/maven/App.java
@@ -0,0 +1,17 @@
+package com.codigofacilito.maven;
+import org.apache.commons.collections4.*
+
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App impeents Unmodifiable
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
